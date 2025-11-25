def003f963dced739fc42c6eee4d1366c27d8a21
Darja Balasheva <balasheva.98@mail.ru>
2025-11-25 18:58:28 +0300

    uncommit

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from back.model.calc_model import Calculator
 
-# val = [0,0,2,1,2,2,3,0]
-# def my_function(a: list):
-#     result = {}
-#     for i in a:
-#         if result.get(i):
-#             result.get(i) += 1
-#         else:
-#             result[i] = 1
-#
+val = [0,0,2,1,2,2,3,0]
+def my_function(a: list):
+    result = {}
+    for i in a:
+        if result.get(i):
+            result.get(i) += 1
+        else:
+            result[i] = 1
+
 
 # Запуск калькулятора
 if __name__ == "__main__":
